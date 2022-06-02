35e55eff6659bc39f3ba29849aa3c26d589e039b
Kristin Glover <klglover@uw.edu>
2022-06-02 13:06:29 -0700

    again

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-
+/* #2 practicing with JavaScript*/
 let myName = "Kristin Glover";
 console.log(myName);
 
-
+/*3 practicing functions in JavaScript*/
 function getVowelCount(aString) {
     let lowerCaseString = aString.toLowerCase();
     let modifiedString = lowerCaseString.replaceAll("a", "").
@@ -18,30 +18,39 @@ function getVowelCount(aString) {
 let numVowelsIsInName = getVowelCount(myName);
 console.log(numVowelsIsInName);
 
+/* #5 defining h1 element and changing text*/
 let h1Element = $('h1');
 h1Element.text("Interactive Pet Viewer");
 
+/* #6 defining footer element and adding content*/  
 let footerElement = $('footer');
 footerElement.html('<small>All images from <a href="https://unsplash.com/">unsplash.com</a></small>');
 
-/*is this right? or should cats be display none?*/
-let catsbut = $('#cats');
-$('#btnshowDogs').click(function() {
-    catsbut.hide();
-});
-$('#btnShowDogs').addClass('active');
-
-let buttonGroup = $('btn-group mb-2');
-buttonGroup.on('click', function() {
-    buttonGroup.toggle();
-});
-buttonGroup.on('click', function() {
-    buttonGroup.toggleClass('active')
+/* #7 attempting to set cats display to none but not working*/
+$('div.#btnShowDogs').on('click', function() {
+    $('div.#cats').hide();
+    $('div.#btnShowDogs').addClass('active');
 });
 
-let imgs = $('#images');
-function imagePointersFunction() {
-    imgs.css('cursor', 'pointer')
+/* #8 trying to add interactivity to buttons group but not working*/
+let buttonGroup = $('div.btn-group');
+buttonGroup.on('click', function() {
+    $('div.#cats').toggle();
+    $('div.#dogs').toggle();
+    buttonGroup.click(function() {
+        buttonGroup.toggleClass('active'); 
+});
+
+/* #9 trying to make pointers hover but not working */
+$('#images').hover(function() {
+    $('#images').css('cursor', 'pointer');
+});
+
+
+/* #12 trying to make images open the modal but not working */
+function modalImages() {
+    imgs.attr("data-bs-toggle-modal");
+    imgs.attr("data-bs-target");    
 };
 
-
+/* #13 specify src alt for the images */
